c686e54d698e05621a6fb161601aebbd3495a167
XiaoMouz <gxiaomouz@gmail.com>
2024-09-02 03:51:15 +0800

    fix: hydration mismatch in Vue 3.4, use radix-vue ConfigProvider to set id
    tips: it should revert in Vue 3.5!!!

--- app.vue
+++ app.vue
@@ -1,8 +1,16 @@
+<script setup lang="ts">
+import { ConfigProvider } from 'radix-vue'
+
+const useIdFunction = () => useId()
+</script>
+
 <template>
-  <NuxtLoadingIndicator />
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <ConfigProvider :use-id="useIdFunction">
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </ConfigProvider>
 </template>
 
 <style>
