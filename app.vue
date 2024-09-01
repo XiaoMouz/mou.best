6fbe49749c8204d089b08639979f8b49dddd1e9a
XiaoMouz <gxiaomouz@gmail.com>
2024-09-02 06:19:23 +0800

    fix: add loading state

--- app.vue
+++ app.vue
@@ -2,14 +2,26 @@
 import { ConfigProvider } from 'radix-vue'
 
 const useIdFunction = () => useId()
+const loading = ref(false)
+
+onMounted(() => {
+  loading.value = true
+})
 </script>
 
 <template>
   <ConfigProvider :use-id="useIdFunction">
-    <NuxtLoadingIndicator />
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+    <div v-show="loading">
+      <NuxtLoadingIndicator />
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
+    <div v-show="!loading">
+      <div class="flex items-center justify-center h-screen bg-black">
+        <LoadingCycle class="border-gray-200" :size-class="`w-10 h-10`" />
+      </div>
+    </div>
   </ConfigProvider>
 </template>
 
