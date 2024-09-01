ce0d3f341254beb3ad35ff832bcd433964f4d1f2
XiaoMouz <gxiaomouz@gmail.com>
2024-09-02 03:52:21 +0800

    fix: in some small size screen have visual question

--- components/home/Menu.vue
+++ components/home/Menu.vue
@@ -4,17 +4,15 @@ const { profile } = await useProfile()
 </script>
 <template>
   <div
-    class="absolute top-[80%] right-[20%] md:top-[40%] z-10 md:right-[10%] flex flex-row md:flex-col items-center justify-center"
+    class="absolute top-[30%] z-10 right-[10%] flex flex-col items-center justify-center"
   >
     <div
-      class="space-x-4 md:space-x-0 md:space-y-4 flex items-center justify-center md:flex-col bg-[#99999970] dark:bg-[#00000070] p-3 rounded-3xl"
+      class="space-y-4 flex items-center justify-center flex-col bg-[#99999970] dark:bg-[#00000070] p-3 rounded-3xl"
     >
       <HomeMenuItem v-for="item in menu" v-bind:item="item" />
     </div>
 
-    <div
-      class="ml-4 md:ml-0 md:mt-4 bg-[#99999970] dark:bg-[#00000070] p-2 rounded-full"
-    >
+    <div class="mt-4 bg-[#99999970] dark:bg-[#00000070] p-2 rounded-full">
       <div
         class="flex py-1 w-10 h-10 items-center justify-center rounded-full transition duration-300 cursor-pointer hover:bg-slate-400 dark:hover:bg-gray-400 fill-gray-900 dark:fill-slate-100 ease-in-out"
       >
