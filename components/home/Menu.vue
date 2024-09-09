12f249958f7dc52f12cc29b7260387c208920c74
XiaoMouz <gxiaomouz@gmail.com>
2024-09-09 10:35:08 +0800

    chore: light and dark mode switch

--- components/home/Menu.vue
+++ components/home/Menu.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
 const menu = useHomeMeta().homeMeta
 const { profile } = await useProfile()
+
+const isDark = useDark()
+console.log(isDark)
 </script>
 <template>
   <div
-    class="absolute top-[30%] z-10 right-[10%] flex flex-col items-center justify-center"
+    class="absolute top-[30%] z-10 right-[10%] flex flex-col items-center justify-center gap-4"
   >
+    <div class="bg-[#99999970] dark:bg-[#00000070] p-2 rounded-full">
+      <div
+        class="flex py-1 w-10 h-10 items-center justify-center rounded-full transition duration-300 cursor-pointer hover:bg-slate-400 dark:hover:bg-gray-400 fill-gray-900 dark:fill-slate-100 ease-in-out"
+        @click="isDark ? (isDark = false) : (isDark = true)"
+      >
+        <Transition name="fade">
+          <IconDark class="absolute" v-if="isDark" />
+          <IconLight class="absolute" v-else />
+        </Transition>
+      </div>
+    </div>
+
     <div
       class="space-y-4 flex items-center justify-center flex-col bg-[#99999970] dark:bg-[#00000070] p-3 rounded-3xl"
     >
       <HomeMenuItem v-for="item in menu" v-bind:item="item" />
     </div>
 
-    <div class="mt-4 bg-[#99999970] dark:bg-[#00000070] p-2 rounded-full">
+    <div class="bg-[#99999970] dark:bg-[#00000070] p-2 rounded-full">
       <div
         class="flex py-1 w-10 h-10 items-center justify-center rounded-full transition duration-300 cursor-pointer hover:bg-slate-400 dark:hover:bg-gray-400 fill-gray-900 dark:fill-slate-100 ease-in-out"
       >
@@ -35,3 +50,14 @@ const { profile } = await useProfile()
     </div>
   </div>
 </template>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
